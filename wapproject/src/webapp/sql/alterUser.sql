fe01523fe7d04cc63e6147daa3318fd14373a461
dzzheng3 <150183186@qq.com>
2017-04-25 17:33:53 -0600

    move

--- wapproject/src/webapp/sql/alterUser.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `carpooling`.`users` 
-CHANGE COLUMN `city` `city` VARCHAR(50) CHARACTER SET 'utf8' NULL ,
-CHANGE COLUMN `street` `street` VARCHAR(50) CHARACTER SET 'utf8' NULL ,
-CHANGE COLUMN `birthyear` `birthyear` INT(4) UNSIGNED NULL ;
